3fbdb40200e54a960f8cb41ac265c4e1572d1aa0
BrandonBrown <brandonbrown@pursuit.org>
2019-11-10 17:20:55 -0500

    question 2

--- lab.js
+++ lab.js
@@ -3,3 +3,19 @@ let userInput = document.querySelector("#string")
 let result = userInput.value
 document.querySelector("#header").innerHTML = result
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let results= [{ "name" : "Anne" },
+    { "name" : "Bri" },
+    { "name" : "Chaz" },
+    { "name" : "Darlene" }]
+    let ul = document.createElement("ul")
+    results.forEach(name => {
+        let li = document.createElement("li")
+        li.innerText = name.name
+        ul.appendChild(li)
+    })
+    document.body.appendChild(ul)
+
+})
+
