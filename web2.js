227d82d1c91465311a24dcf5f13636cd56038eeb
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-11-11 16:03:22 -0500

    finished q2

--- web2.js
+++ web2.js
@@ -1,6 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let people = {
-
-    }
-    let ul = document.createElement("ul")
+document.addEventListener("DOMContentLoaded", () =>{
+    let results = [
+        { "name" : "Anne" },
+        { "name" : "Bri" },
+        { "name" : "Chaz" },
+        { "name" : "Darlene" }
+      ];
+      let ul = document.createElement("ul")
+      results.forEach(el =>{
+          let li = document.createElement("li")
+          li.innerText = el.name
+          ul.appendChild(li)
+      })
+      document.body.appendChild(ul)
 })
+console.log("hello")
