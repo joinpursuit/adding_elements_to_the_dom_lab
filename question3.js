1d1382de08b0e39eaca062d8b1c458ad4219a144
DejaLFlynn <dejaflynn@pursuit.org>
2019-11-10 20:41:14 -0500

    question3

---
+++ question3.js
@@ -0,0 +1,111 @@
+  
+document.addEventListener("DOMContentLoaded", () => {
+
+    let obj = {
+        "results": [
+          {
+            "gender": "female",
+            "name": {
+              "title": "mademoiselle",
+              "first": "ruth",
+              "last": "nicolas"
+            },
+            "nat": "CH"
+          },
+          {
+            "gender": "female",
+            "name": {
+              "title": "miss",
+              "first": "رها",
+              "last": "سلطانی نژاد"
+            },
+            "nat": "IR"
+          },
+          {
+            "gender": "female",
+            "name": {
+              "title": "mrs",
+              "first": "patricia",
+              "last": "hale"
+            },
+            "nat": "GB"
+          },
+          {
+            "gender": "male",
+            "name": {
+              "title": "mr",
+              "first": "fernando",
+              "last": "cooper"
+            },
+            "nat": "US"
+          },
+          {
+            "gender": "female",
+            "name": {
+              "title": "mrs",
+              "first": "یسنا",
+              "last": "صدر"
+            },
+            "nat": "IR"
+          },
+          {
+            "gender": "male",
+            "name": {
+              "title": "mr",
+              "first": "eino",
+              "last": "tuomala"
+            },
+            "nat": "FI"
+          },
+          {
+            "gender": "female",
+            "name": {
+              "title": "ms",
+              "first": "gonca",
+              "last": "özkara"
+            },
+            "nat": "TR"
+          },
+          {
+            "gender": "male",
+            "name": {
+              "title": "mr",
+              "first": "kyle",
+              "last": "castillo"
+            },
+            "nat": "US"
+          },
+          {
+            "gender": "female",
+            "name": {
+              "title": "miss",
+              "first": "olivia",
+              "last": "kumar"
+            },
+            "nat": "NZ"
+          },
+          {
+            "gender": "male",
+            "name": {
+              "title": "monsieur",
+              "first": "raymond",
+              "last": "durand"
+            },
+            "nat": "CH"
+          }
+        ],
+        "info": {
+          "seed": "2cb086ce097c87ee",
+          "results": 10,
+          "page": 1,
+          "version": "1.2"
+        }
+      }
+     let results = obj.results
+     console.log(results)
+     for(let key of results){
+         let name = document.createElement("li")
+         name.innerText = `${key.nat} ${key.name.first } ${key.name.last}`
+         document.body.appendChild(name)
+     }
+ })
