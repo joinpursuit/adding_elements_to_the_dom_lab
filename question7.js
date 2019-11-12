48907d8763d6f54726b182b610a69794a5d17b15
DejaLFlynn <dejaflynn@pursuit.org>
2019-11-12 08:38:33 -0500

    more of question 7

--- question7.js
+++ question7.js
@@ -2,13 +2,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
   
     let button = document.querySelector('#button')
     button.addEventListener('click', ()=>{
-        let listItems = document.querySelector('#number')
-        let firstInput = document.createElement('P')
-        firstInput.innerText = listItems.value
-        let secondInput = document.createElement('P')
-        secondInput.innerText = listItems.value
-        let thirdInput = document.createElement('P')
-        thirdInput.innerText = listItems.value
+        let listItems = document.createElement('ul')
+        listItems.innerText = 'list numbers'
+        const inputNumbers = ()=>{
+            document.body.appendChild(listItems)
+            let number = parseInt(document.querySelector('#number').value)
+            if(!isNaN(number)){
+                let newItem = document.createElement('li')
+                newItem.innerText = number
+                listItems.appendChild(newItem)
+            }
+            let everyNumber = document.querySelectorAll('li')
+                if(everyNumber.length>0){
+                    let average = 0
+                    let max = -Infinity
+                    let min = Infinity
+                }
+        }
 
+        let firstInput = document.createElement('p1')
+        firstInput.innerText = listItems.value
+        let secondInput = document.createElement('p2')
+        secondInput.innerText = listItems.value
+        let thirdInput = document.createElement('p3')
+        thirdInput.innerText = listItems.value
+        let averageInput = document.createElement('p')
+        let maxInput = document.createElement('p')
+        let minInput = document.createElement('p')
     })
 })
