ee0f8a431c4861146b1559cea009e7f6d0efcb5b
Henrynunez112 <henrynunez@pursuit.org>
2019-11-11 20:47:24 -0500

    done question 3

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const addStr = () =>{
 }
 // QUESTION: 2
 document.addEventListener("DOMContentLoaded",() =>{
-let obj = {
-    "results" : [
+let results =
+ [
     { "name" : "Anne" },
     { "name" : "Bri" },
     { "name" : "Chaz" },
     { "name" : "Darlene" }
   ]
-}
 let ul = document.createElement("ul");
 
-obj.results.forEach(names  => {
+results.forEach(names  => {
   let li = document.createElement("li")
   li.innerText = names["name"]
   ul.appendChild(li)
@@ -27,42 +26,109 @@ document.body.appendChild(ul)
 
 // QUESTION: 3
 document.addEventListener("DOMContentLoaded",() => {
-  let obj2 = {
-    "results": [
-      {
-        "gender": "female",
-        "name": {
-          "title": "mademoiselle",
-          "first": "ruth",
-          "last": "nicolas"
-        },
-        "nat": "CH"
+  let results2 =
+  [
+    {
+      "gender": "female",
+      "name": {
+        "title": "mademoiselle",
+        "first": "ruth",
+        "last": "nicolas"
       },
-      {
-        "gender": "female",
-        "name": {
-          "title": "miss",
-          "first": "رها",
-          "last": "سلطانی نژاد"
-        },
-        "nat": "IR"
+      "nat": "CH"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "miss",
+        "first": "رها",
+        "last": "سلطانی نژاد"
       },
-      {
-        "gender": "female",
-        "name": {
-          "title": "mrs",
-          "first": "patricia",
-          "last": "hale"
-        },
-        "nat": "GB"
-      }
-    ]
-  }
+      "nat": "IR"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "patricia",
+        "last": "hale"
+      },
+      "nat": "GB"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "fernando",
+        "last": "cooper"
+      },
+      "nat": "US"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "یسنا",
+        "last": "صدر"
+      },
+      "nat": "IR"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "eino",
+        "last": "tuomala"
+      },
+      "nat": "FI"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "ms",
+        "first": "gonca",
+        "last": "özkara"
+      },
+      "nat": "TR"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "kyle",
+        "last": "castillo"
+      },
+      "nat": "US"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "miss",
+        "first": "olivia",
+        "last": "kumar"
+      },
+      "nat": "NZ"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "monsieur",
+        "first": "raymond",
+        "last": "durand"
+      },
+      "nat": "CH"
+    }
+  ]
+
   let ul = document.createElement("ul");
 
-  obj2.results.forEach(el  => {
+  results2.forEach(el  => {
     let li = document.createElement("li")
-    li.innerText = el["name","nat"]
+    li.innerText = `${el["nat"]} ${el["name"]["first"]} ${el["name"]["last"]}`
     ul.appendChild(li)
 
+  })
+document.body.appendChild(ul)
 })
+
+//question 4
