22dafd5bbe8733aa2344b2dcfd4b6d57c0173cf2
Danielle R Cherry <daniellecherry@pursuit.org>
2019-11-11 22:01:23 -0500

    Complete Question Five

---
+++ questionFive.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    const createHtml = () => {
+        let textBox = document.createElement("input")
+        textBox.createAttribute("type", "text")
+        textBox.appendChild(type, text)
+    }
+    document.body.createHtml
+    document.body.appendChild(textBox)
+})
