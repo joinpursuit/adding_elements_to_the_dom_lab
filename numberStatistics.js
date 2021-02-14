66f5c0df34a20546527dc87ba440a783b666ef82
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-02-14 17:22:06 -0500

    addingElementsLab

--- numberStatistics.js
+++ numberStatistics.js
@@ -32,20 +32,3 @@ const numStats = () => {
   minimum.textContent = min;
   maximum.textContent = max;
 };
-
-//     let averageInput = Number(document.getElementById("number-statistics-average").value);
-//     let maxInput = Number(document.getElementById("number-statistics-maximum").value);
-//     let minInput = Number(document.getElementById("number-statistics-minimum").value);
-//     let numInput = Number(document.getElementById("number-statistics-number-input").value);
-//     let li = document.createElement("li")
-//     li.textContent = numInput
-//     list.appendChild(li)
-//     let list = document.getElementById("number-statistics-numbers");
-//     let min = Infinity
-//     for (let i = 0; i < numInput; i++) {
-//         if (numInput < min) {
-//             min = numInput[i]
-//         }
-//         minInput.textContent = min
-//     }
-// }
