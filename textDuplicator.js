cd437e6396c674ac67bb429f5c43f4533b368023
joshuamendez <joshuafmendez@gmail.com>
2021-02-14 11:10:36 -0500

    completed textDuplicator problem and passed

--- textDuplicator.js
+++ textDuplicator.js
@@ -1,3 +1,9 @@
-const duplicate = () => {
-    
+const duplicator = () => {
+    const stringInput = document.querySelector('#text-duplicator-string-input').value;
+    const numberInput = Number(document.querySelector('#text-duplicator-number-input').value);
+    for(let i = 0; i < numberInput;i++){
+        const li = document.createElement('li');
+        li.textContent = stringInput;
+        document.querySelector('#text-duplicator-list').appendChild(li);
+    }
 }
