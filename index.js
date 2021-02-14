32ae0989053b099a30e7c517ad5182d76f029118
Miguel Catala <miguelcatala27@yahoo.com>
2021-02-14 15:58:25 -0500

    Lab complete

--- index.js
+++ index.js
@@ -32,3 +32,121 @@ const numStat = () => {
         document.getElementById('number-statistics-minimum').innerText = `The min is:${Math.min(...minMaxArray)}`;
      
     }
+  
+    const objectUserList = {
+    "results": [
+        {
+        "gender": "female",
+        "name": {
+            "title": "mademoiselle",
+            "first": "ruth",
+            "last": "nicolas"
+        },
+        "nat": "CH"
+        },
+        {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "رها",
+            "last": "سلطانی نژاد"
+        },
+        "nat": "IR"
+        },
+        {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "patricia",
+            "last": "hale"
+        },
+        "nat": "GB"
+        },
+        {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "fernando",
+            "last": "cooper"
+        },
+        "nat": "US"
+        },
+        {
+        "gender": "female",
+        "name": {
+            "title": "mrs",
+            "first": "یسنا",
+            "last": "صدر"
+        },
+        "nat": "IR"
+        },
+        {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "eino",
+            "last": "tuomala"
+        },
+        "nat": "FI"
+        },
+        {
+        "gender": "female",
+        "name": {
+            "title": "ms",
+            "first": "gonca",
+            "last": "özkara"
+        },
+        "nat": "TR"
+        },
+        {
+        "gender": "male",
+        "name": {
+            "title": "mr",
+            "first": "kyle",
+            "last": "castillo"
+        },
+        "nat": "US"
+        },
+        {
+        "gender": "female",
+        "name": {
+            "title": "miss",
+            "first": "olivia",
+            "last": "kumar"
+        },
+        "nat": "NZ"
+        },
+        {
+        "gender": "male",
+        "name": {
+            "title": "monsieur",
+            "first": "raymond",
+            "last": "durand"
+        },
+        "nat": "CH"
+        }
+    ],
+    "info": {
+        "seed": "2cb086ce097c87ee",
+        "results": 10,
+        "page": 1,
+        "version": "1.2"
+    }
+    };
+
+    function userListRandom(array) {
+        const list = document.createElement('li')
+        list.innerText = `${array.name.first} ${array.name.last} (${array.nat})`;
+        return list;
+    }
+    const users = objectUserList.results;
+    const ulList = document.getElementById('user-list');
+    users.forEach(array => {
+        const callUserList = userListRandom(array);
+        ulList.appendChild(callUserList);
+    });
+    
+    
+    
+    
+    
