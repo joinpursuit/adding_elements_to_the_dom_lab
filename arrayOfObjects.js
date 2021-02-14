3a785c6caa08f6f89473d91fbb4a7d3b6dcc4241
poonam <poonam398@gmail.com>
2021-02-14 15:27:08 -0500

    userList

---
+++ arrayOfObjects.js
@@ -0,0 +1,111 @@
+const arrayOfObjects =
+  [
+      {
+        "gender": "female",
+        "name": {
+          "title": "mademoiselle",
+          "first": "ruth",
+          "last": "nicolas"
+        },
+        "nat": "CH"
+      },
+      {
+        "gender": "female",
+        "name": {
+          "title": "miss",
+          "first": "رها",
+          "last": "سلطانی نژاد"
+        },
+        "nat": "IR"
+      },
+      {
+        "gender": "female",
+        "name": {
+          "title": "mrs",
+          "first": "patricia",
+          "last": "hale"
+        },
+        "nat": "GB"
+      },
+      {
+        "gender": "male",
+        "name": {
+          "title": "mr",
+          "first": "fernando",
+          "last": "cooper"
+        },
+        "nat": "US"
+      },
+      {
+        "gender": "female",
+        "name": {
+          "title": "mrs",
+          "first": "یسنا",
+          "last": "صدر"
+        },
+        "nat": "IR"
+      },
+      {
+        "gender": "male",
+        "name": {
+          "title": "mr",
+          "first": "eino",
+          "last": "tuomala"
+        },
+        "nat": "FI"
+      },
+      {
+        "gender": "female",
+        "name": {
+          "title": "ms",
+          "first": "gonca",
+          "last": "özkara"
+        },
+        "nat": "TR"
+      },
+      {
+        "gender": "male",
+        "name": {
+          "title": "mr",
+          "first": "kyle",
+          "last": "castillo"
+        },
+        "nat": "US"
+      },
+      {
+        "gender": "female",
+        "name": {
+          "title": "miss",
+          "first": "olivia",
+          "last": "kumar"
+        },
+        "nat": "NZ"
+      },
+      {
+        "gender": "male",
+        "name": {
+          "title": "monsieur",
+          "first": "raymond",
+          "last": "durand"
+        },
+        "nat": "CH"
+      }
+    ]
+
+
+arrayOfObjects.forEach(user=>{
+  let li= document.createElement("li")
+
+  let first = user["name"]['first']
+  let last = user["name"]["last"]
+  let nat = user["nat"]
+  
+  li.textContent = `${first} ${last} (${nat})`
+
+  document.querySelector("#user-list").appendChild(li) 
+})
+
+
+
+
+  //module.exports = arrayOfObjects
