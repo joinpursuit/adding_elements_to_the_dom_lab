7623f1a511b0d9d9676cf8b57d49cda98e2514af
poonam <poonam398@gmail.com>
2021-02-14 17:31:05 -0500

    removed console logs in numberStatistics

--- numberStatistics.js
+++ numberStatistics.js
@@ -5,51 +5,38 @@ const numberStatistics = () => {
   liTag.textContent = Number(userInput);
   document.querySelector("#number-statistics-numbers").appendChild(liTag);
 
-  const listItems = document.querySelectorAll(".numbers");
-  console.log(listItems);
-  
+  const listItems = document.querySelectorAll(".numbers");  
   
   let count = 0;
   let sum = 0;
   for (const li of listItems) {
     if (li) {
       count++;
-      console.log(count);
     }
   }
 
   for (const li of listItems) {
     if (li) {
       sum += Number(li.textContent);
-      console.log(sum);
     }
   }
-  let avg = sum / count;
-  console.log(avg);
 
-  document.querySelector(
-    "#number-statistics-average"
-  ).textContent = `This is the avg ${avg}`;
+  let avg = sum / count;
+  document.querySelector("#number-statistics-average").textContent = `This is the avg ${avg}`;
   
   let min = Infinity;
   for (const li of listItems) {
     if (li.textContent < min) {
       min = Number(li.textContent);
-      document.querySelector(
-        "#number-statistics-minimum"
-      ).textContent = `This is the min number ${min}`;
+      document.querySelector("#number-statistics-minimum").textContent = `This is the min number ${min}`;
     }
   }
-  console.log(typeof min)
 
   let max = -Infinity;
   for (const li of listItems) {
     if (li.textContent > max) {
       max = Number(li.textContent);
-      document.querySelector(
-        "#number-statistics-maximum"
-      ).textContent = `This is the max number ${max}`;
+      document.querySelector("#number-statistics-maximum").textContent = `This is the max number ${max}`;
     }
   }
-  console.log(typeof max)
 };
