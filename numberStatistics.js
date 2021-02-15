d4b478dc0e116b75327f122734154e4c14c088c1
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-02-15 15:43:27 -0500

    numberStatistics not solved, more information needed

---
+++ numberStatistics.js
@@ -0,0 +1,23 @@
+function statistics(){
+    const number = document.getElementById('number-statistics-number-input')
+    const num = number.value
+   
+    
+
+let ave = 0
+for (let i=1;i<=num;i++){
+    ave += i
+}
+
+ave = parseInt(ave/num).toFixed(0)
+
+const average = document.querySelector("#number-statistics-average")
+average.textContent = ave
+
+const maximun = document.querySelector("#number-statistics-maximum")
+maximun.textContent = num
+
+const minimum = document.querySelector("#number-statistics-minimum")
+minimum.textContent = num/num
+
+}
