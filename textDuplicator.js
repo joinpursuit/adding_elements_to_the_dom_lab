53ff52b3ed8b5381b9919d4387b294258e6f8552
Coreen Cooper <coreencooper@pursuit.org>
2021-02-14 20:52:18 -0500

    add numberStatistics1()

--- textDuplicator.js
+++ textDuplicator.js
@@ -1,10 +1,10 @@
 const textDuplicator = () => {
-    let str = document.querySelector("#text-duplicator-string-input").value
-    let num = document.querySelector("#text-duplicator-number-input").value
+    let str = document.querySelector("#text-duplicator-string-input").value;
+    let num = Number(document.querySelector("#text-duplicator-number-input").value);
     
-    for(let i=0; i<Number(num); i++){
-         let li=document.createElement("li")
-         li.textContent = str
-         document.querySelector('#text-duplicator-list').appendChild(li)
+    for (let i = 0; i< num; i++) {
+         let li = document.createElement("li"); // create <li>
+         li.textContent = str // assign the text of <li>
+         document.querySelector("#text-duplicator-list").appendChild(li); // add <li> to <ul>
     }
 }
