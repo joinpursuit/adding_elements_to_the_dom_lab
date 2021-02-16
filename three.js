13eca40f4ca2538b15a164e7dc07b20af851774c
Jamee Hengari <jhengari@hotmail.com>
2021-02-15 19:58:15 -0500

    first adding elements dom lab commit

---
+++ three.js
@@ -0,0 +1,104 @@
+const userCreator = (user) => {
+  return `${user.name.first} ${user.name.last} (${user.nat})`;
+};
+
+const users = [
+  {
+    gender: "female",
+    name: {
+      title: "mademoiselle",
+      first: "ruth",
+      last: "nicolas",
+    },
+    nat: "CH",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "miss",
+      first: "رها",
+      last: "سلطانی نژاد",
+    },
+    nat: "IR",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "patricia",
+      last: "hale",
+    },
+    nat: "GB",
+  },
+  {
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "fernando",
+      last: "cooper",
+    },
+    nat: "US",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "یسنا",
+      last: "صدر",
+    },
+    nat: "IR",
+  },
+  {
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "eino",
+      last: "tuomala",
+    },
+    nat: "FI",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "ms",
+      first: "gonca",
+      last: "özkara",
+    },
+    nat: "TR",
+  },
+  {
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "kyle",
+      last: "castillo",
+    },
+    nat: "US",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "miss",
+      first: "olivia",
+      last: "kumar",
+    },
+    nat: "NZ",
+  },
+  {
+    gender: "male",
+    name: {
+      title: "monsieur",
+      first: "raymond",
+      last: "durand",
+    },
+    nat: "CH",
+  },
+];
+
+const listElement = document.getElementById("user-list");
+
+users.forEach((elem) => {
+    let newList = document.createElement("li")
+    newList.textContent = userCreator(elem)
+    listElement.appendChild(newList)
+});
