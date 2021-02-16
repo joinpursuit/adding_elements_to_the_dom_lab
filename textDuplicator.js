a9ba69326876f44bf31a2dd7aaf1250d36cdec37
Jon Narine <jonnicwolf@gmail.com>
2021-02-16 16:33:02 -0500

    line 2 (#text-dupli...)=>(text-dupli...)

--- textDuplicator.js
+++ textDuplicator.js
@@ -1,15 +1,21 @@
 const duplicator = () => {
     const input = document.querySelector('#text-duplicator-string-input').value
-    let inputNum = parseInt(document.getElementById('#text-duplicator-number-input'))
-    console.log(inputNum)
-    console.log(typeof inputNum)
+    let inputNum = document.getElementById('#text-duplicator-number-input').value
+    let inputNumConvert = inputNum *= 1
+    console.log(inputNumConvert)
+    console.log(typeof inputNumConvert)
+    
     let output = document.querySelector('#text-duplicator-list')
 
     let li = document.createElement('li')
     li.textContent = input;
-                    
-    output.appendChild(li)
-    console.log(input)
+    
+    console.log(li)
+    for (let i = 0; i < inputNumConvert; i++){
+        output.appendChild(li)
+        console.log(input)
+    }
+    
 
 }
 
