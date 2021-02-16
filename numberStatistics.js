9fccd9c281c885feca11e6357adc50a08427e0b8
Carlo <carlonyc001@gmail.com>
2021-02-16 17:56:01 -0500

    adding elements to the dom lab complete

--- numberStatistics.js
+++ numberStatistics.js
@@ -1,22 +1,6 @@
-//Make a website (`numberStatistics.html`) with a number input, a button, and 3 paragraphs.  Clicking on the button should add the number to a list.  The first paragraph should show the average of all the numbers, the second paragraph should show the maximum value, and the third paragraph should show the minimum value.
 
-// //1. add a number to a list
-// 2.how to get the numbers in the list? loop?
-// 3. get numbers in list and find avergae
-// 4. get numbers in list and get max
-// 5. get numbers in list and get the min value
 
-// const duplicate = () => {
-//     const textInput = document.querySelector("#text-duplicator-string-input");
-//     const numInput = document.querySelector("#text-duplicator-number-input");
-//     let list = document.querySelector("#text-duplicator-list");
-//     let num2 = Number(numInput.value);
-//     debugger;
-//     for (let i = 0; i < num2; i++) {
-//       let li = document.createElement("li");
-//       li.textContent = textInput.value;
-//       list.appendChild(li);
-//     }
+
 
 const expectStatistics = () => {
   const number = Number(
@@ -32,6 +16,7 @@ const expectStatistics = () => {
   let list = document.querySelectorAll("#number-list li");
   p1.textContent = average(list);
   p2.textContent = maximum(list);
+  p3.textContent = minimum(list)
   //for (let value of number) {
   //let li = document.createElement("li");
 };
@@ -58,13 +43,15 @@ const maximum = (list) => {
   return maxNum;
 };
 
-const maximum = (list) => {
-  let maxNum = 0;
+const minimum = (list) => {
+  let minNum = Infinity;
   for (let value of list) {
-    if (Number(value.textContent) > maxNum) {
+    if (Number(value.textContent) < minNum) {
       console.log(value);
-      maxNum = Number(value.textContent);
+      minNum = Number(value.textContent);
     }
   }
-  return maxNum;
+  return minNum;
 };
+
+
