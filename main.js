bd11d0deceb8ff6eafc188c785e4e08b13b71101
DamienYule <damienyule@gmail.com>
2021-02-17 17:33:12 -0500

    Problem 2

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
+//////////Problem 1/////////////
 const textDuplicator = () => {
-    const input = document.getElementById('text-duplicator-string-input')
-    const inputVal = input.value 
-    const inputNum = document.getElementById('text-duplicator-number-input')
-    const inputNumVal = Number(inputNum.value)
-    for(let i = 0 ; i < inputNumVal; i++){
+    const inputVal = document.getElementById('text-duplicator-string-input').value
+    const inputNumVal = Number(document.getElementById('text-duplicator-number-input').value)
+    for (let i = 0; i < inputNumVal; i++) {
         let newLi = document.createElement("li")
         newLi.textContent = inputVal
         let listEl = document.getElementById('text-duplicator-list')
         listEl.appendChild(newLi)
     }
 }
-// let lesterListItem = document.createElement("li");
-// lesterListItem.textContent = "Jon Lester (L) P";
-// let listElement = document.getElementById("lineupList");
-// listElement.appendChild(lesterListItem);
-
-// Clicking the submit button will add that many copies of the string as
-// list items to an unordered list with id='text-duplicator-list'
+///////////Problem 2//////////////
+//global object
+let obj = {
+    storedVal: [],
+    total: 0,
+    maxNum: -Infinity,
+    minNum: Infinity
+}
+const numberStatistics = () => {
+    //grab number input 
+    const inputNum = Number(document.getElementById('number-statistics-number-input').value)
+    //store value in global variable to know length for average calculation
+    obj.storedVal.push(inputNum)
+    //store total in global variable for average calculation  
+    obj.total += inputNum
+    //create average variable from global variables 
+    let avgNum = obj.total / obj.storedVal.length
+    //change global max value 
+    if (inputNum > obj.maxNum) {
+        obj.maxNum = inputNum
+    }
+    //change global min value
+    if (inputNum < obj.minNum) {
+        obj.minNum = inputNum
+    }
+    //select dom elements 
+    const averagePTag = document.getElementById('number-statistics-average')
+    const maximumPTag = document.getElementById('number-statistics-maximum')
+    const minimumPTag = document.getElementById('number-statistics-minimum')
+    //change textcontent of dom elements
+    averagePTag.textContent = `Avg: ${avgNum}`
+    maximumPTag.textContent = `Max: ${obj.maxNum}`
+    minimumPTag.textContent = `Min: ${obj.minNum}`
+    //list sumbmisions
+    let newLi = document.createElement("li")
+    newLi.textContent = inputNum
+    let domLocation = document.getElementById('number-statistics-numbers')
+    domLocation.appendChild(newLi)
+}
