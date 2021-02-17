d856f8a0051a292139c69a0b2fbbcb225ab78a76
cassidybeni <cassidybeni@pursuit.org>
2021-02-16 21:39:05 -0500

    Complete textDuplicator.html

--- index.js
+++ index.js
@@ -40,7 +40,17 @@ const numberStats = () => {
     min.innerText = minVal
 }
 
-const textDup = () => {}
+const textDup = () => {
+    let input1 = document.querySelector('#text-duplicator-string-input').value
+    let input2 = Number(document.querySelector('#text-duplicator-number-input').value)
+    let ul = document.querySelector('#text-duplicator-list')
+    
+    for(let i = 0; i < input2; i++){
+        let li = document.createElement('li')
+        li.innerText = input1
+        ul.appendChild(li)
+    }
+}
 
 const starWars = () => {}
 
