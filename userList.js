853dde677ab1f56e77b660777fa77e0f5c029a51
Du Min Chen <dm.chen0113@gmail.com>
2021-02-16 20:55:27 -0500

    Lab complete

---
+++ userList.js
@@ -0,0 +1,109 @@
+const data = {
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "mademoiselle",
+        "first": "ruth",
+        "last": "nicolas"
+      },
+      "nat": "CH"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "miss",
+        "first": "رها",
+        "last": "سلطانی نژاد"
+      },
+      "nat": "IR"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "patricia",
+        "last": "hale"
+      },
+      "nat": "GB"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "fernando",
+        "last": "cooper"
+      },
+      "nat": "US"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "mrs",
+        "first": "یسنا",
+        "last": "صدر"
+      },
+      "nat": "IR"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "eino",
+        "last": "tuomala"
+      },
+      "nat": "FI"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "ms",
+        "first": "gonca",
+        "last": "özkara"
+      },
+      "nat": "TR"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "mr",
+        "first": "kyle",
+        "last": "castillo"
+      },
+      "nat": "US"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "miss",
+        "first": "olivia",
+        "last": "kumar"
+      },
+      "nat": "NZ"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "monsieur",
+        "first": "raymond",
+        "last": "durand"
+      },
+      "nat": "CH"
+    }
+  ],
+  "info": {
+    "seed": "2cb086ce097c87ee",
+    "results": 10,
+    "page": 1,
+    "version": "1.2"
+  }
+}
+
+window.onload = function() {
+  const ol = document.querySelector("#user-list")
+  for(const user of data.results){
+    const li = document.createElement("li")
+    li.textContent = `${user.name.first} ${user.name.last} (${user.nat})`
+    ol.appendChild(li)
+  }
+}
