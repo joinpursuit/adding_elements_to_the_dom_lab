86891fcf8ca4cc301fca805cde61fdd0a30dd635
Alexander <alexanderl96@yahoo.com>
2021-02-17 16:25:16 -0500

    3

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+
+let sum = 0
+let counter = 0
+let min = Infinity
+let max = -Infinity
 function numberStatic(){
+    
     // 84 get added in the ul
       // get users input from the DOM
 const num = document.getElementById('number-statistics-number-input').value
@@ -11,16 +17,43 @@ const num = document.getElementById('number-statistics-number-input').value
       const list = document.getElementById('number-statistics-numbers')
       // append our new li  to the ul
       list.appendChild(li)
+let avg = 0
+ sum += num1
+ counter++
+ avg = Math.floor(sum/counter)
+ if (num1 < min){
+     min = num1
+ }
+ if(num1 > max){
+     max=num1
+ }
+ console.log(sum)
+
+
+const p1 = document.getElementById('number-statistics-minimum')
+p1.textContent= `Minimum: ${min}`
+const p2 = document.getElementById('number-statistics-maximum')
+p2.textContent = `Maximum: ${max}`
+const p3 = document.getElementById('number-statistics-average')
+p3.textContent =`Average: ${avg}`
+
+
+
+
+
 
 
     // update average
-    const allParas = document.querySelectorAll('li')
-    for(let el of allParas){
-        
-        el.textContent = num1
-        console.log('el')
-        allParas.appendChild = el
-    }
+    
+    
     //update min
+    // const minimum = document.getElementById('id="number-statistics-minimum')
+    // let min = num1[0]
+    // for(let i = 0; i <num1.length;i++){
+    //     if(arr[i]< min){
+    //         min = arr[i]
+    //     }
+    // }return min
+
     //update max
 }
