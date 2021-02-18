d9051c5540d4a64c2c91154a117663f5e6606615
Jenna Qian <jiayin1996@gmail.com>
2021-02-18 12:25:24 -0500

    userList

--- numberStatistics.js
+++ numberStatistics.js
@@ -43,128 +43,39 @@ const numberStats = () => {
 
 
 
+// let array = []
 
+// function statistics() {
+//   const number = Number(document.querySelector("#number-statistics-number-input").value)
+//   const average = document.querySelector("#number-statistics-average")
+//   const max = document.querySelector("#number-statistics-maximum")
+//   const min = document.querySelector("#number-statistics-minimum")
+//   const list = document.querySelector("#number-statistics-numbers")
 
+//   let maxValue = -Infinity
+//   let minValue = Infinity
+//   let averageValue = 0
+//   let sum = 0
 
-// let newArray = []
+//   array.push(number)
 
-// const getAverage = () => {
-//     let num = Number(document.querySelector("#number-statistics-number-input").value)
-//     let uList = document.querySelector('#number-statistics-numbers')
-//     let average = document.querySelector('#number-statistics-average')
-//     let max = document.querySelector('#number-statistics-maximum')
-//     let min = document.querySelector('#number-statistics-minimum')
+//   for(let i = 0; i < array.length; i++) {
+//   sum += array[i]
+//   }
 
-//     let li = document.createElement('li')
-//     li.innerText = num
-//     uList.appendChild(li)
+//   let sortedArray = array.sort((a,b) => {
+//     return a-b
+//   })
 
-//     newArray.push(num)
+//   averageValue = sum/array.length
+//   maxValue = sortedArray[sortedArray.length-1]
+//   minValue = sortedArray[0]
 
-//     //Average 
-//     const findAverage = () => {
-//         let sum = 0
-//         newArray.forEach = ( (n) => {
-//             sum += n
-//             let avg = sum /newArray.length
-//             return avg
-//         })
-//         // for(let i = 0;i < newArray.length; i++){
-//         //     sum += newArray[i]
-//         //     let avg = sum/newArray.length
-//         //     return avg
-//         // }
-//     }
-//     findAverage()
-    
-//     average.innerText = avg
-//     //equal sign reassign the value avg
+//   const newLi = document.createElement("li")
+//   newLi.textContent = number
+//   list.appendChild(newLi)
 
-
-//     //Max
-//     const findMaximum = () => {
-//         let maximum = Math.max(...newArray)   
-//         return maximum
-
-//     }
-//     findMaximum()
-
-//     max.innerText = maximum
-    
-//     //Min
-//     const findMinimum = () => {
-//         let minimum = Math.min(...newArray)
-//         return minimum
-//     }
-//     findMinimum()
-
-//     min.innerText= minimum
-
+//   average.textContent = `Average is ${averageValue}`
+//   max.textContent = `Max is ${maxValue}`
+//   min.textContent = `Min is ${minValue}`
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------------
-
-
-// const getAverage = () => {
-//     //get users input from the DOM
-//     const input = document.querySelector('#number-statistics-number-input')
-//     const value = input.value
-//     // const value = Number(document.querySelector('#number-statistics-number-input').value)
-
-//     //convert users input to a number
-//     const num = Number(value)
-//     // const num2 = parseInt(value)
-//     // console.log(`num1: ${num2} is a ${typeof num2}`)
-    
-//     //create a new li and set its text content to the users input 
-//     const li = document.createElement('li')
-//     li.textContent = num
-
-//     //get the ul from the DOM
-//     const list = document.querySelector('#numbers')
-//     console.log(list)
-
-//     //append our new li to the ul
-//     list.appendChild(li)
-
-//     //update average
-//     //update min
-//     //update max 
-// }
-
-
-
-
-//get users input from the DOM
-    // const input = document.querySelector('#number-statistics-number-input')
-    // const value = input.value
-    // const num1 = Number(value)
-//one line code
-    // const num = Number(document.querySelector('#number-statistics-number-input').value)
-
-
-
-
-//Get user input from the DOM
-    //use document.querySelector or document.getelementbyID
-//Transformed that input (converted it to a number)
-    //this step depends on what problem your're trying to solve
-//Created a new DOM element and set its textContent to our transformed input 
-    //(Our new li element)
-//Grabbed a parent element from the DOM
-    //grab the ul from the DOM
-//We append our new DOM element to the parent 
-    //call ul.appendChild 
