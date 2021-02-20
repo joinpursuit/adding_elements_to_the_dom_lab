b12ca563163b9f875d98703b2a4a88e1b58e99c1
Flower Aviles <itsfloweraviles@gmail.com>
2021-02-19 21:52:06 -0500

    finally found the bug. It was a misspelling on the word TEXT

--- textDuplicator.js
+++ textDuplicator.js
@@ -1,6 +1,6 @@
 const textDuplicator = () => {
     const stringInput = document.querySelector("#text-duplicator-string-input").value;
-    const numberInput = Number(document.querySelector("#tex-duplicator-number-input").value);
+    const numberInput = Number(document.querySelector("#text-duplicator-number-input").value);
     for (let i = 0; i < numberInput; i++) {
         const li = document.createElement ("li");
         li.textContent = stringInput;
