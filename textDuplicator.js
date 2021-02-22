f3bec5b5aad36b3426f3594bbe5fa8bc20489d8a
Candice Agard <candiceagard@Candices-MacBook-Pro.local>
2021-02-22 00:15:56 -0600

    Text duplicator solution

---
+++ textDuplicator.js
@@ -0,0 +1,12 @@
+const duplicator = () => {
+    let inputText = document.getElementById("text-duplicator-string-input");
+    let inputTextValue = inputText.value;
+    let inputNumber = document.getElementById("text-duplicator-number-input");
+    let inputNumValue = inputNumber.value;
+    let listElement = document.getElementById("text-duplicator-list");
+    for (let i = 0; i < inputNumValue; i++) {
+      let newList = document.createElement("li");
+      newList.textContent = inputTextValue;
+      listElement.appendChild(newList);
+    }
+}
