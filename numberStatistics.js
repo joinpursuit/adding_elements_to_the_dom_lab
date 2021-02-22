5790b7100c2e15bc218ab300457990b673ce57b0
Hady Mohamed <hadymohamed@pursuit.org>
2021-02-21 22:24:10 -0500

    finished lab

---
+++ numberStatistics.js
@@ -0,0 +1,50 @@
+// function onSubmit() {
+//      const input = document.querySelector('#number-statistics')
+//      const value = input.value
+//      const num = Number(value)
+//      const li = document.createElement('li')
+//      li.textContent = num
+//      const list = document.querySelector('#numbers')
+//      list.appendChild(li)
+
+
+
+// }
+
+let array = []
+
+function statistics() {
+
+const number = Number(document.querySelector("#number-statistics-number-input").value)
+const average = document.querySelector("#number-statistics-average")
+const max = document.querySelector("#number-statistics-maximum")
+const min = document.querySelector("#number-statistics-minimum")
+const list = document.querySelector("#number-statistics-numbers")
+
+let maxValue = -Infinity
+let minValue = Infinity
+let averageValue = 0
+let sum = 0
+
+array.push(number)
+
+for(let i = 0; i < array.length; i++) {
+sum += array[i]
+}
+
+let sortedArray = array.sort((a,b) => {
+  return a-b
+})
+
+averageValue = sum/array.length
+maxValue = sortedArray[sortedArray.length-1]
+minValue = sortedArray[0]
+
+const newLi = document.createElement("li")
+newLi.textContent = number
+list.appendChild(newLi)
+
+average.textContent = `Average is ${averageValue}`
+max.textContent = `Max is ${maxValue}`
+min.textContent = `Min is ${minValue}`
+}
