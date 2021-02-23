7f400fbb88030a5ae0ca1eaff46c8aaa0514d6e0
Jon Narine <jonnicwolf@gmail.com>
2021-02-23 00:32:00 -0500

    accessed obj | grab first & last name key next

--- userList.js
+++ userList.js
@@ -95,19 +95,27 @@ const obj = {    //obj.results[0].name.title === madmoiselle
 }
 console.log(obj)
 
-const kawolski = () => {
-    //const li = document.createElement('li')
-    let input = obj.results[0].name
-    console.log(input)
-    const output = document.querySelector('#user-list')
-    output.appendChild(li)
-    
-}
-kawolski()
+// const kawolski = () => {
+//   //const li = document.createElement('li')
+//   let input = obj.results[0].name
+//   console.log(input)
+//   //const output = document.querySelector('#user-list')
+//   //output.appendChild(li)
+//   let vals = Object.values(input)
+//   console.log(vals)
+// }
+// kawolski()
 console.log(obj.results[0].name.title)
+const input = obj.results
+console.log(obj.results)
+for (let i = 0; i < input.length; i++) {
+  let nameInput = input[i].name
+  let nameArr = Object.values(nameInput)
+  console.log(nameArr.length)
+  console.log(input.length)
 
-
-
+ 
+}
 
 
 
