26c27a4b9c276d3a55c01f15989c14fde576ffce
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-02-23 23:19:00 -0500

    question 3 done

--- numberStatistics.js
+++ numberStatistics.js
@@ -1,4 +1,4 @@
-function numberStatistics(){
+ function numberStatistics(){
     // 1.grap the users input
     const input= document.querySelector('#number-statistics-number-input').value
     //const value =input.value(save as value)
@@ -39,4 +39,21 @@ function numberStatistics(){
     maximum.textContent= `This is the max ${max}`;
     const minimum=document.querySelector('#number-statistics-minimum');
     minimum.textContent= `This is the min ${min}`;
-}
+} 
+
+//const inputArr = []
+//const numberSnek = () => {
+   // const input = Number(document.querySelector('#number-statistics-number-input').value);
+   // const ul = document.querySelector('#number-statistics-numbers')
+   // let avg = document.querySelector('#number-statistics-average')
+   // let max = document.querySelector('#number-statistics-maximum')
+   // let min = document.querySelector('#number-statistics-minimum')
+   // inputArr.unshift(input)
+  //  let li = document.createElement('li')    
+   // li.textContent = input
+   // ul.appendChild(li)
+  //  let sorter = inputArr.sort((lo, hi) => { return hi - lo })     
+   // max.textContent = `Maximum: ${sorter[0]}`
+  //  min.textContent = `Minimum: ${sorter[sorter.length - 1]}`    
+  //  avg.textContent = `Average: ${sorter.reduce((a, b) => { return (a + b) })/sorter.length}`    
+//}
---
+++ userList.js
@@ -0,0 +1,102 @@
+const getNation =(user)=>{
+    return `${user.name.first} ${user.name.last} (${user.nat})`
+}
+const allUsers =[
+
+        {
+          "gender": "female",
+          "name": {
+            "title": "mademoiselle",
+            "first": "ruth",
+            "last": "nicolas"
+          },
+          "nat": "CH"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "miss",
+            "first": "رها",
+            "last": "سلطانی نژاد"
+          },
+          "nat": "IR"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "mrs",
+            "first": "patricia",
+            "last": "hale"
+          },
+          "nat": "GB"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "mr",
+            "first": "fernando",
+            "last": "cooper"
+          },
+          "nat": "US"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "mrs",
+            "first": "یسنا",
+            "last": "صدر"
+          },
+          "nat": "IR"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "mr",
+            "first": "eino",
+            "last": "tuomala"
+          },
+          "nat": "FI"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "ms",
+            "first": "gonca",
+            "last": "özkara"
+          },
+          "nat": "TR"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "mr",
+            "first": "kyle",
+            "last": "castillo"
+          },
+          "nat": "US"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "miss",
+            "first": "olivia",
+            "last": "kumar"
+          },
+          "nat": "NZ"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "monsieur",
+            "first": "raymond",
+            "last": "durand"
+          },
+          "nat": "CH"
+        }
+      ]
+    const listElement =document.getElementById('user-list')
+    for(const user of allUsers){
+        const newUserListItem =document.createElement('li')
+        newUserListItem.textContent =getNation(user)
+        listElement.appendChild(newUserListItem)
+    } 
